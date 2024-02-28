e44c7beadf8f7b03f2eba426f9473a1fb0c947ac
Midhula A <142242670+Midhula0@users.noreply.github.com>
2024-02-28 19:49:15 +0530

    Add files via upload

---
+++ LastDigit.java
@@ -0,0 +1,16 @@
+//LAST DIGIT
+
+import java.util.Scanner;  
+public class LastDigit  
+{  
+public static void main(String args[])   
+{  
+int num=509876;
+//loop start execution form and execute until the condition i<=10 becomes false  
+int digit=0;
+digit=num%10;
+
+System.out.println(digit);  
+}  
+}  
+ 
