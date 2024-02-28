9b670644b17d41442c6a66b29eb37caad1b449f8
Midhula A <142242670+Midhula0@users.noreply.github.com>
2024-02-28 19:52:12 +0530

    Add files via upload

---
+++ SecondLast.java
@@ -0,0 +1,10 @@
+ //SECOND LAST DIGIT
+public class SecondLast{
+    public static void main(String[] args){
+        int a = 120045;
+        int secondLastDigit = (a % 100) / 10;
+        System.out.println(secondLastDigit);
+        
+    }
+}
+ 
