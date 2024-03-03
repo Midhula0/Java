17f5708a9f4154f0784a8f8bfe897873ef6aa630
Midhula A <142242670+Midhula0@users.noreply.github.com>
2024-03-03 23:13:18 +0530

    Add files via upload

---
+++ CharToString.java
@@ -0,0 +1,8 @@
+//CHARACTER TO STRING
+
+public class CharToString{  
+public static void main(String args[]){  
+char c='S';  
+String s=String.valueOf(c);  
+System.out.println("String is: "+s);  
+}}
