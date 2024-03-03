e7e403a6b7d69704274e9747c00a3a9ab0a814b1
Midhula A <142242670+Midhula0@users.noreply.github.com>
2024-03-03 23:09:43 +0530

    Add files via upload

---
+++ StringToChar.java
@@ -0,0 +1,8 @@
+//String to character
+
+public class StringToChar{  
+public static void main(String args[]){  
+String s="hello";  
+char c=s.charAt(0);//returns h  
+System.out.println("1st character is: "+c);  
+}} 
